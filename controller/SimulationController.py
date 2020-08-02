95a07fd074ec7d111e0bfbf88c8c6f10f2bde600
Mreza Gh <mrezagh.legend@gmail.com>
2020-08-02 16:24:51 +0430

    :hammer: complete step 1&3 of SimulationFlowServiceImpl

--- controller/SimulationController.py
+++ controller/SimulationController.py
@@ -3,7 +3,7 @@ from service.simulationService import SimulationFlowServiceImpl, InitializationS
 
 class SimulationController:
 
-    def __init__(self, number_of_patients=10**7):
+    def __init__(self, number_of_patients=10 ** 7):
         self.number_of_patients = number_of_patients
 
     def start_process(self):
@@ -15,20 +15,21 @@ class SimulationController:
         4- introducing the results to analyzer
         5- showing the results
         """
-        # todo implement
         simulation_parameters = self.get_inputs()
         initializer = InitializationServiceImpl(simulation_parameters, self.number_of_patients)
         simulation_context = initializer.initialize_simulation()
-        # TODO: steps 3 to 5
+        simulator = SimulationFlowServiceImpl()
+        simulator.run_simulation(simulation_context)  # TODO: half of step 3
+        # TODO: steps 4 to 5
 
         pass
 
     @staticmethod
     def get_inputs():
         # M, landa, alpha, mu = map(float, input().split())
-        number_of_rooms, patient_arrival_rate, average_patience, reception_service_rate,  = map(float, input().split())
+        number_of_rooms, patient_arrival_rate, average_patience, reception_service_rate, = map(float, input().split())
         number_of_rooms = int(number_of_rooms)
         doctors_service_rate = []
         for room_mu_s in range(number_of_rooms):
             doctors_service_rate.append(list(map(float, input().split())))
-        return number_of_rooms, patient_arrival_rate, average_patience,  reception_service_rate, doctors_service_rate
+        return number_of_rooms, patient_arrival_rate, average_patience, reception_service_rate, doctors_service_rate
--- service/simulationService/SimulationFlowServiceImpl.py
+++ service/simulationService/SimulationFlowServiceImpl.py
@@ -2,23 +2,24 @@ from queue import Queue
 from heapq import heapify, heappush, heappop
 
 from model.dto.SimulationContext import SimulationContext
+from model.Worker import Receptionist, Doctor
 
 
 class SimulationFlowServiceImpl:
 
     def __init__(self):
-        self.time = 0
+        self.timer = 0
         self.current_patient_index = 0
 
     def run_simulation(self, simulation_context: SimulationContext):
 
         simulationFinished = False
-        timer = 0
         matrix = simulation_context.simulation_matrix
         event_list = []
         reception_queue = Queue()  # this queue only holds the IDs of patients not the actual object
+        self.initialize_event_list(simulation_context, event_list)
 
-        while not self.check_simulation_finish():
+        while not self.check_simulation_finish(event_list, matrix):
             # todo implement
             """
             while flow:
@@ -43,16 +44,47 @@ class SimulationFlowServiceImpl:
                 3A- if it is empty, simulationFinished = true
                 3B- else set the timer to next soonest event
             """
+            self.timer = self.next_soonest_event(event_list=event_list)  # step 3 done
             self.insert_patients(reception_queue, matrix)
+            current_event_list = self.build_current_event_list(event_list)  # step 1 done
+            for time, event in current_event_list:  # step 2:
+                if isinstance(event, Receptionist):
+                    # TODO: implement step 2A
+                    pass
+                elif isinstance(event, Doctor):
+                    # TODO: implement step 2B
+                    pass
 
-            pass
         return matrix
 
-    def check_simulation_finish(self):
-        # TODO: implement
-        pass
+    def check_simulation_finish(self, event_list, matrix):
+        if not event_list and self.current_patient_index == len(matrix):
+            return True
+        return False
+
+    def build_current_event_list(self, event_list):
+        current_event_list = []
+        while event_list and event_list[0][0] == self.timer:
+            current_event_list.append(heappop(event_list))
+        return current_event_list
 
     def insert_patients(self, reception_queue, matrix):
-        while matrix[self.current_patient_index]['arrival_time'] <= self.time:
+        while matrix[self.current_patient_index]['arrival_time'] <= self.timer:
             reception_queue.put(self.current_patient_index)
             self.current_patient_index += 1
+
+    def initialize_event_list(self, simulation_context, event_list):
+        receptionist = simulation_context.hospital.receptionist
+        heappush(event_list, (0, receptionist))
+
+        for room in simulation_context.hospital.rooms:
+            for doctor in room.doctors:
+                heappush(event_list, (0, doctor))
+
+    def next_soonest_event(self, event_list):
+        if event_list:
+            timer = event_list[0][0]
+        else:
+            timer = self.timer + 1
+        return timer
+
