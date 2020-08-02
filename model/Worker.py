e75fe194e6f6ae26b16d610b6e781918d7a522b6
Mreza Gh <mrezagh.legend@gmail.com>
2020-08-02 16:08:41 +0430

    :sparkles: make doctor & receptionist comparable

--- model/Worker.py
+++ model/Worker.py
@@ -1,3 +1,6 @@
+from functools import total_ordering
+
+
 class Worker:
     def __init__(self, distribution):
         self.distribution = distribution
@@ -21,11 +24,30 @@ class Worker:
         pass
 
 
+@total_ordering
 class Receptionist(Worker):
     def __init__(self, distribution):
         super(Receptionist, self).__init__(distribution)
 
+    def __lt__(self, other):
+        if not isinstance(other, Worker):
+            raise Exception("bro you are comparing the wrong thing, chill")
+        if isinstance(other, Doctor):
+            return False
+        return True
 
+
+@total_ordering
 class Doctor(Worker):
     def __init__(self, distribution):
         super(Doctor, self).__init__(distribution)
+
+    def __lt__(self, other):
+        if not isinstance(other, Worker):
+            raise Exception("bro you are comparing the wrong thing, chill")
+        if isinstance(other, Receptionist):
+            return True
+        return False
+
+
+
